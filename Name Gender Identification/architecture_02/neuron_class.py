fccd0b71f68b6c0de3f3b01b548a2ec0ba1d4ae8
Willian-Girao <williansoaresgirao@gmail.com>
2019-04-01 13:23:22 -0300

    Neuron class updated

--- Name Gender Identification/architecture_02/neuron_class.py
+++ Name Gender Identification/architecture_02/neuron_class.py
@@ -5,30 +5,36 @@ from math import e
 from evolve_filters import Population
 
 class Neuron:
-    def __init__(self, e, class_1_filter, class_2_filter, class_1_label, class_2_label,
-    training_set_size, number_of_traning_steps):
-        self.euler_number = e
-        self.class_1_filter = class_1_filter # Patterns for class 1
-        self.class_2_filter = class_2_filter # Patterns for class 2
-        self.class_1_label = class_1_label # Label for class 1
-        self.class_2_label = class_2_label # Label for class 2
-        self.n_ij = 0.0 # Prediction certainty
-        self.p_k = [] # Number of correct answers during training step k - [0,...,k].
-        self.t_k = training_set_size # Number of samples in the training set k.
-        self.lambda = number_of_traning_steps # Defined number of traning rounds.
+    def __init__(self, class_1_filter, class_2_filter, class_1_label, class_2_label):
+        self.class_1_filter = class_1_filter # Patterns for class 1.
+        self.class_2_filter = class_2_filter # Patterns for class 2.
+        self.class_1_label = class_1_label # Label for class 1.
+        self.class_2_label = class_2_label # Label for class 2.
+        self.n_ij = 0.0 # Prediction certainty.
+        self.predicted_label = "none" # The label the neuron has predicted.
+        self.sig_p_nxj = 0.0 # Activation function value.
 
-    # Calculates an activation function that decides if the neuron should fire or not.
-    # @n_ij_sum - the sum of all prediction certainties of the neurons within layer j.
-    # @p_cik - the sum of all
-    # @quocient_j - sum of all dividers for each neuron within layer j.
-    def checkActivation(self, n_ij_sum, quocient_j):
-        a = self.n_ij / n_ij_sum
-        b_aux = 0.0
-        for i in range(len(self.p_k)):
-            b_aux += self.p_k[i] / self.t_k
-        b = b_aux / self.lambda
+    # Calculates the averge matchs in 'target' using 'class_filter'.
+    # @class_filter - set of characters to be matched.
+    # @target - string where the patterns will try to be matched against.
+    def classPrediction(class_filter, target):
+        match_avg = 0.0
+        for chr in class_filter:
+            if chr['pattern'] in target:
+                match_avg += 1.0
+        return (match_avg / len(class_filter))
 
-        divider_j = (a * b)
-        x = divider_j / quocient_j
+    # Decides which label should be given to the target sample to be classified.
+    # @target - data to be classified.
+    def predict(self, target):
+        class_1_certainty = classPrediction(self.class_1_filter, target)
+        class_2_certainty = classPrediction(self.class_2_filter, target)
 
-        return 1 / (1 + (self.euler_number ** (-x)))
+        self.n_ij = max(class_1_certainty, class_2_certainty)
+
+        if class_1_certainty == class_2_certainty:
+            self.predicted_label = "undecidable"
+        elif class_1_certainty > class_2_certainty:
+            self.predicted_label = self.class_1_label
+        else:
+            self.predicted_label = self.class_2_label
